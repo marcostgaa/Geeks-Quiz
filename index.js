4704d3abd3a3b22510b2f99f728106c29e256943
gabizinhagalo <150306362+gabizinhagalo@users.noreply.github.com>
2023-11-29 12:36:27 -0300

    js do index

---
+++ index.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    // Recuperar informações do localStorage
+    var loggedIn = localStorage.getItem('loggedIn');
+    var currentUser = localStorage.getItem('currentUser');
+    var userImage = localStorage.getItem('userImage');
+
+    // Verificar se o usuário está logado
+    if (loggedIn === 'true' && currentUser) {
+        // Exibir nome do usuário
+        $('#userName').text('Bem-vindo, ' + currentUser);
+
+        // Exibir imagem do usuário (se existir)
+        if (userImage) {
+            $('#profileImage').attr('src', userImage);
+        }
+    } else {
+        // Redirecionar para a página de login se o usuário não estiver logado
+        window.location.href = 'login.html';
+    }
+});
+
+function toggleSidebar() {
+    const sidebar = $('#mySidebar');
+    const isOpen = sidebar.css('left') === '0';
+
+    sidebar.css('left', (sidebar.css('left') === '-200px') ? '0' : '-200px');
+
+    if (!isOpen) {
+        var loggedIn = localStorage.getItem('loggedIn');
+        var currentUser = localStorage.getItem('currentUser');
+        var userImage = localStorage.getItem('userImage');
+
+        // Verificar se o usuário está logado
+        if (loggedIn === 'true' && currentUser) {
+            // Exibir nome do usuário
+            $('#userName').text(currentUser);
+
+            // Exibir imagem do usuário (se existir)
+            if (userImage) {
+                $('#profileImage').attr('src', userImage);
+            }
+        }
+    }
+}
+
+function changeNameColor() {
+    const profileName = $('#userName');
+    const colorPicker = $('#colorPicker');
+
+    profileName.css('color', colorPicker.val());
+}
+
+function openColorPicker() {
+    const colorPicker = $('#colorPicker');
+
+    // Abre a paleta de cores
+    colorPicker.click();
+}
